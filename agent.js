5b6847afb77ff6fc30b85a63e4e7eb5991279f47
fragro <fragro@fragro-computa.(none)>
2012-09-14 10:31:51 -0500

    update

--- agent.js
+++ agent.js
@@ -41,7 +41,7 @@ var agent = (function() {
 	    		$('#wait').remove();
 	    		var link = '<div class="rec" id="' + data.result.results[i].id +'"><a href="' + data.result.results[i].url +'" target="_blank">' + data.result.results[i].title + '</a></div>';
 				var keyword =  '<div class="keyword">Keyword: ' + data.result.results[i].keyword + '</div>'
-				var rating =  '<div id="star' + window.urlCount  + '"></div>'
+				var rating =  '<div class="star-rate" id="star' + window.urlCount  + '"></div>'
 				var summary = '<div class="summary">' + data.result.results[i].summary + '<br>' + rating + '<br>' + keyword + '</div>'
 				$(div).append(link);
 				$(div).append(summary);
