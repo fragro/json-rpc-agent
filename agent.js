34338a2641cfcfb41ddefff739aaae864c71ad4f
fragro <fragro@fragro-computa.(none)>
2012-09-14 04:23:50 -0500

    updates

--- agent.js
+++ agent.js
@@ -4,44 +4,19 @@
 * generates recommendations on the fly as the user requests more. Optionally the user can interact with the
 * agent, sending back ratings or other types of feedback which in turn drives a collaborative filtering process.
 */
-var agent = {
-	version:"0.1.0.1",	
-	requestCount: 0,
-	debug: true,
-	div:'#recommendation',
 
-	//public functions
-	subscribe: function() {
-		this._send_request('subscribe', [this._userID, this._user_profile], this._successCallback, this._errorCallback);
-	},
-
-	recommendation: function() {
-		this._send_request('recommendation', [this._userID], this._appendRec, this._errorCallback);
-	},
-
-	rate: function(indexkey, rating) {
-		this._send_request('rate', [this._userID, indexkey, rating], this._successCallback, this._errorCallback);
-	},
-
-    init: function (serviceUrl, options) {
-		//we need to support cross-domain requests since this is loaded on nutraspace server.
-		//need to test extensively on IE
-		jQuery.support.cors = true;
-        //this.url = 'http://localhost:8080/jsonrpc';
-        this._url = serviceUrl.clone();
-        this._userID = options.userID.clone();
-        this._user_profile = options.profile.clone();
-		this.div = options.div.clone();
-        console.log(this.div);
-        //initialized now subscribe the user to the service and grab recommendations
-        this.subscribe();
-        this.recommendation();
-    },
+var agent = (function() {
+	var version = "0.1.0.1";
+	var requestCount = 0;
+	var debug =  true;
+	var div = '#recommendation';
+	var _url;
+	var _userID;
+	var _user_profile;
 
     //private functions
-
     //successcallback for reccomendation
-    _appendRec: function(data){
+    this._appendRec = function(data){
     	for(var i in data.result.results){
     		var st = '<div class="rec" id="' + data.result.results[i].id +'"><a href="' + data.result.results[i].url +'" target="_blank">' + data.result.results[i].title + '</a></div>';
 			console.log(st);
@@ -50,7 +25,7 @@ var agent = {
     	}
     },
 
-    _send_request: function (method, params, successCallback, errorCallback) {
+    this._send_request = function (method, params, successCallback, errorCallback) {
     	var randomID=Math.floor(Math.random()*11100)
          $.ajax({
             url: this._url, 
@@ -59,17 +34,45 @@ var agent = {
             dataType:"json",
             success:  function (data) { successCallback(data); },
             error: function (err)  { errorCallback(data); }
-
      	});
-
     },
     //test callback functions for debugging
-	_successCallback: function(data) {
+	this._successCallback = function(data) {
 		console.log(JSON.stringify(data));
 	},
 
-	_errorCallback: function(err) {
+	this._errorCallback = function(err) {
 		console.log(JSON.stringify(err));
 	},
 
-}
+
+	return {
+		//public functions
+		this.subscribe = function() {
+			this._send_request('subscribe', [this._userID, this._user_profile], this._successCallback, this._errorCallback);
+		},
+
+		this.recommendation = function() {
+			this._send_request('recommendation', [this._userID], this._appendRec, this._errorCallback);
+		},
+
+		this.rate = function(indexkey, rating) {
+			this._send_request('rate', [this._userID, indexkey, rating], this._successCallback, this._errorCallback);
+		},
+
+	    this.init = function (serviceUrl, options) {
+			//we need to support cross-domain requests since this is loaded on nutraspace server.
+			//need to test extensively on IE
+			jQuery.support.cors = true;
+	        //this.url = 'http://localhost:8080/jsonrpc';
+	        _url = serviceUrl;
+	        this._userID = options.userID;
+	        this._user_profile = options.profile;
+			this.div = options.div;
+	        console.log(this.div);
+	        //initialized now subscribe the user to the service and grab recommendations
+	        this.subscribe();
+	        this.recommendation();
+	    },
+	}
+})();
