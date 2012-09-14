d5f57bb8d8f60e10807801b9676c7d485fe0b896
fragro <fragro@fragro-computa.(none)>
2012-09-14 03:54:06 -0500

    updates

--- agent.js
+++ agent.js
@@ -41,7 +41,7 @@ var agent = {
     //successcallback for reccomendation
     _appendRec: function(data){
     	for(var i in data.result.results){
-	    	console.log(i);
+	    	console.log(data.result.results.i);
     		var st = '<div class="rec" id="' + i.id +'"><a href="' + i.url +'" target="_blank">' + i.title + '</a></div>';
 			console.log(this._div);
 			console.log(st);
