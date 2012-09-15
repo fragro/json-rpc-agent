640deb032b6932e902e055a9713e57242c4b1b82
fragro <fragro@fragro-computa.(none)>
2012-09-14 22:13:55 -0500

    update

--- agent.js
+++ agent.js
@@ -86,7 +86,7 @@ var agent = (function() {
     function _appendRec(data, div){
     	if(data.result.results.length == 0){
     		$('#wait').html("Recommendations are being generated.<br> In a moment click 'More Recommendations'");
-    		this.recommendation();
+    		that.recommendation();
 			$(div).append(link);
     	}
     	else{
