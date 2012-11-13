309879d80a7122c8ec8c72918ba3de78852f0758
fragro <fragro@fragro-computa.(none)>
2012-11-13 16:14:46 -0600

    update

--- agent.js
+++ agent.js
@@ -122,7 +122,7 @@ var agent = (function() {
 	function parseSearchData(data){
 		console.log(data);
 		for(hit in data.hits.hits){
-			console.log(hit._source);
+			console.log(hit.type);
 			if(hit._source._cls == 'Base.PubMed'){
 				//render pubmed template
 				_append('#pubmed', hit._source, '#publications');
