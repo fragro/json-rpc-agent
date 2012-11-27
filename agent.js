c5f62accb18c436aefe61a703a7891e0d7801880
fragro <fragro@fragro-computa.(none)>
2012-11-27 11:40:02 -0600

    update

--- agent.js
+++ agent.js
@@ -149,14 +149,12 @@ function agent(serviceUrl, options){
 				else if(source._cls == 'Base.Asset'){
 					_render('#node', source, '#overview');
 					for(var i = 0; i < source.elements.length; i++){
-						console.log(source.elements[i]);
 						var s = {'name': source.elements[i].name, 'child': ''};
 						var subE = source.elements[i].subElements;
 						for(var j = 0; j < subE.length; i++){
 							s['description'] += '<h4>' + subE[j].title  + '</h4>'
 						}
-						console.log(s)
-						_append('#node', s, '#recommendations');
+						//_append('#node', s, '#recommendations');
 
 					}
 				}
