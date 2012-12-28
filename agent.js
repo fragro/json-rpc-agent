c9765720374ef73245bfcbda81529bf412175db1
fragro <fragro@fragro-computa.(none)>
2012-12-28 06:17:38 -0600

    ;update

--- agent.js
+++ agent.js
@@ -262,6 +262,7 @@ function agent(serviceUrl, options){
         		}
             	if(source._cls == "MedLine"){
             		for(var key in source.site_keys){
+            			console.log(source.site_keys[key]);
             			_append('#key_medline', source.site_keys[key], '#sink_key_' + source._id);
             		}
 					_append('#tab_' + source._cls, source, '#sink_tab_' + source._cls);
