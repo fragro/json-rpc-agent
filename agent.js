b5dbbec8220dd6791b95023c5a236da6b84df30e
fragro <fragro@fragro-computa.(none)>
2013-01-24 15:06:56 -0600

    update to ne ES client

--- agent.js
+++ agent.js
@@ -344,7 +344,7 @@ function agent(serviceUrl, options){
 		api({'index': 'genindex', 'type': 'gendoc', 'query': query}, 'description', {'size' : this.querySize});
 
 		//api({'index': 'aisle7index', 'type': 'asset', 'query': query}, 'description', {'size' : 10});
-		//api({'index': 'nutraindex', 'type': 'node', 'query': query}, 'title', {'size' : 1});
+		api({'index': 'nutraindex', 'type': 'node', 'query': query}, 'title', {'size' : 1});
 		api({'index': 'pubmedindex', 'type': 'pubmed', 'query': query}, 'description');
 		//api({'index': 'nutraindex', 'type': 'medline', 'query': query}, 'description', {'size' : 10});
 		api({'index': 'drugindex', 'type': 'rx', 'query': query}, 'description');
